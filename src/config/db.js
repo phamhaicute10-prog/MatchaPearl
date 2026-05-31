4474ea8ac65108dcb6a0347f8aa3b30b61af6898
haikunn11 <phamhaicute9@gmail.com>
2026-05-31 19:12:30 +0700

    Fix Aiven SSL and Port

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'matcha_pearl_db',
+    ssl: process.env.DB_HOST ? { rejectUnauthorized: false } : null,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
