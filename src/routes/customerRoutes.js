2a2ebe6f8958d15220fe5a5904170ed41c817313
haikunn11 <phamhaicute9@gmail.com>
2026-06-03 21:58:15 +0700

    Fix requireAuth in customerRoutes

--- src/routes/customerRoutes.js
+++ src/routes/customerRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const customerController = require('../controllers/customerController');
-const { requireAuth } = require('../middlewares/authMiddleware');
 
-router.get('/', requireAuth, customerController.getCustomers);
-router.get('/search', requireAuth, customerController.getCustomerByPhone);
-router.post('/', requireAuth, customerController.createCustomer);
+router.get('/', customerController.getCustomers);
+router.get('/search', customerController.getCustomerByPhone);
+router.post('/', customerController.createCustomer);
 
 module.exports = router;
